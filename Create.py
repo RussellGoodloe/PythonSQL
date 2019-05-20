4fd4eac5b406e2a9a96b5dddf1d4593a02232e2d
Russell Goodloe <russell@ugollc.com>
2019-05-20 14:30:25 -0500

    All queries working, code fresh and clean

--- Create.py
+++ Create.py
@@ -2,36 +2,23 @@ import DB
 import CustomerFile
 import datetime
 
-from DB import ReadData
+from DB import ReadData as DBReadData, WriteData as DBWriteData, SelectData as DBSelectData
 from CustomerFile import Customer
 from datetime import date
 
 def AddCustomer(johnDoe):
-    print ('started add to customer')
     AddtoCustomer(johnDoe)
-    print ('passed add to cust. Started custlist')
     AddToCustomerListID(johnDoe)
-    print ('pass that, on to emails')
     AddToEmail(johnDoe)
-    print ('passed that, on to numbers')
     AddToPhoneNumbers(johnDoe)
-    print ('passed that, on to addresses')
     AddToAddresses(johnDoe)
 
 def AddtoCustomer(johnDoe):
-    #default = 'INSERT INTO Customer(CustID, Created, LastSeen, TotalNoOfOrders, BasketSum, BasketAvg, PriceSum, PriceAvg, CustStatus, CustName) VALUES'
-    #print ('add check 1')
-    #unique = '('+str(johnDoe.CustID) +','+str(johnDoe.Created)+','+str(johnDoe.LastSeen)+','+str(johnDoe.TotalNoOfOrders)+','+str(johnDoe.BasketSum)+','+str(johnDoe.BasketAvg)+','+str(johnDoe.PriceSum)+','+str(johnDoe.PriceAvg)+','+str(johnDoe.CustStatus)+','+str(johnDoe.CustName)+')'
-    #unique = '''(1116969,date(2018,5,5),date(2019,4,8),25,100,4,600,24,'Active','John Doe')'''
-    #print ('add check 2')
-    query = '''INSERT INTO Customer(CustID, Created, LastSeen, TotalNoOfOrders, BasketSum, BasketAvg, PriceSum, PriceAvg, CustStatus, CustName) VALUES 
-                ('''+str(johnDoe.custID)+''','2018-04-08','2019-05-07',25,100,4,600,24,'Active','John Doe')'''
-    #query = default + unique
-    #print (query)
-    query2 = 'SELECT * FROM Customer'
-    DB.SelectData(query2)
-    print ('check')
-    DB.WriteData(query)
+    default = 'INSERT INTO Customer(CustID, Created, LastSeen, TotalNoOfOrders, BasketSum, BasketAvg, PriceSum, PriceAvg, CustStatus, CustName) VALUES'
+    unique = "("+str(johnDoe.custID) +",'"+ToAcceptableDate(johnDoe.created)+"','"+ToAcceptableDate(johnDoe.lastSeen)+"',"+str(johnDoe.totalNoOfOrders)+","+ str(johnDoe.basketSum)+","+str(johnDoe.basketAvg)+","+str(johnDoe.priceSum)+","+ str(johnDoe.priceAvg)+",'"+johnDoe.custStatus+"','"+johnDoe.custName+"')"
+    query = default + unique
+    DB.SelectData('SELECT * FROM Customer')
+    DBWriteData(query)
 
 def AddToCustomerListID(johnDoe):
     d1 = date(2015,1,1)
@@ -41,28 +28,22 @@ def AddToCustomerListID(johnDoe):
     d5 = date(2019,1,1)
     custListID = '2015' if d1 <= johnDoe.created < d2 else '2016' if d2 <= johnDoe.created < d3 else '2017' if d3 <= johnDoe.created < d4 else '2018' if d4 <= johnDoe.created < d5 else '2019'
     query = 'INSERT INTO Customer_ListID(ListID, CustID) VALUES (' + custListID + ',' + str(johnDoe.custID)+')'
-    DB.WriteData(query)
+    DBWriteData(query)
 
 def AddToEmail(johnDoe):
-    default = 'INSERT INTO Customer_Email(Email, CustID) VALUES'
-    for email in johnDoe.emails.emails:
-        unique = '('+email+ ',' +str(johnDoe.custID)+')'
-        query = default + unique
-        DB.WriteData(query)
+    default = 'INSERT INTO Customer_Email(CustID, Email) VALUES '
+    for email in johnDoe.emails.emails:        
+        DBWriteData(default + '(' + str(johnDoe.custID) + ",'"+ email +"')")
 
 def AddToPhoneNumbers(johnDoe):
-    default = 'INSERT INTO Customer_PhoneNo(PhoneNo, CustId) VALUES'
+    default = 'INSERT INTO Customer_PhoneNo(PhoneNo, CustId) VALUES '
     for number in johnDoe.numbers.numbers:
-        unique = '('+str(number)+','+str(johnDoe.custID)+')'
-        query = default + unique
-        DB.WriteData(query)
+        DBWriteData(default + '('+str(number)+','+str(johnDoe.custID)+')')
 
 def AddToAddresses(johnDoe):
-    default = 'INSERT INTO Customer_Address(StreetAddress, City, CState, ZipCode, CustId) VALUES'
+    default = 'INSERT INTO Customer_Address(StreetAddress, City, CState, ZipCode, CustId) VALUES '
     for address in johnDoe.addresses.addresses:
-        unique = '('+ address.streetAddress+','+address.city+','+address.state+','+str(address.zipCode)+','+str(johnDoe.custID) +')'
-        query = default + unique
-        DB.WriteData(query)
+        DBWriteData(default + "('"+ address.streetAddress+"','"+address.city+"','"+address.state+"',"+str(address.zipCode)+','+str(johnDoe.custID) +')')
 
 def WriteToCustomerList():
     query = '''INSERT INTO CustomerList(ListID, Title) VALUES
@@ -71,4 +52,10 @@ def WriteToCustomerList():
             ('2017', 'title2?'),
             ('2018', 'another2?'),
             ('2019', 'again')'''
-    DB.WriteData(query)
+    DBWriteData(query)
+
+def ToAcceptableDate(temp):
+    return str(temp.year) + '-' + TwoDig(temp.month) + '-' + TwoDig(temp.day)
+
+def TwoDig(number):
+    return str(number) if number >= 10 else '0'+str(number)
--- Main.py
+++ Main.py
@@ -10,36 +10,53 @@ from datetime import date
 
 def __main__():
 
-    emails = ['''johnD@ugollc.com''','''JohhnyBoy@gmail.com''']
-    emailList = CustomerEmails(1116969,emails)
-    address1 = Address('1431 Hackberry Ln','Tuscaloosa','AL',35401)
-    address2 = Address('1613 11th st','Tuscaloosa','AL',35401)
-    addresses = [address1,address2]
-    addressList = CustomerAddresses(1116969,addresses)
-    phoNo = 2054318071
-    phoNo2 = 2514087651
-    phones = [phoNo,phoNo2]
-    phoneList = CustomerPhoneNumbers(1116969, phones)
-    johnDoe = Customer(1116969,date(2018,5,5),date(2019,4,8),25,100,4,600,24,'Active','John Doe',emailList,addressList,phoneList)
-    Create.AddCustomer(johnDoe)
     try:
         #Create.WriteToCustomerList()
         Create.AddCustomer(johnDoe)
-        print ('no way')
+        print ('shit worked bro')
     except:
         print ('no dice brah')
     input()
 
-# def PrintCustomers(allCustomers):
+
+if __name__ == "__main__":
+    __main__()
+
+
+#######################################################
+#--Just prints a little sample data for all customer
+#######################################################
+
+# def PrintCustomers(allCustomers):         
 #     for customer in allCustomers:
 #         print (customer.ToStringSmall(customer))
 
-# def MakeThemCustomers(oldList):
+
+
+########################################################
+#--Turns datareasults into customers
+########################################################
+
+# def MakeThemCustomers(oldList):           
 #     customers = []
 #     for columnResult in oldList:
 #         customers.append(Customer(columnResult[0],columnResult[1],columnResult[2],columnResult[3],columnResult[4],columnResult[5],columnResult[6], columnResult[7],columnResult[8],columnResult[9]))
 #     return customers
 
 
-if __name__ == "__main__":
-    __main__()
+#######################################################
+#- Test data to manually create a customer
+#######################################################
+
+
+    # emails = ['johnD@ugollc.com','JohhnyBoy@gmail.com']   
+    # emailList = CustomerEmails(1116969,emails)
+    # address1 = Address('1431 Hackberry Ln','Tuscaloosa','AL',35401)
+    # address2 = Address('1613 11th st','Tuscaloosa','AL',35401)
+    # addresses = [address1,address2]
+    # addressList = CustomerAddresses(1116969,addresses)
+    # phoNo = 2054318071
+    # phoNo2 = 2514087651
+    # phones = [phoNo,phoNo2]
+    # phoneList = CustomerPhoneNumbers(1116969, phones)
+    # johnDoe = Customer(1116969,date(2018,5,5),date(2019,4,8),25,100,4,600,24,'Active','John Doe',emailList,addressList,phoneList)
